498db03d264053ef482f014cf74f036c3954ff92
Partha Mitra <ppm2k2@gmail.com>
2024-08-12 12:19:25 -0400

    Fixed Service End Point Name

--- FundAdminRestAPI/EndPoints/FundService.cs
+++ FundAdminRestAPI/EndPoints/FundService.cs
@@ -7,7 +7,7 @@ namespace FundAdminRestAPI.EndPoints
     {
         public static void MapFundServiceEndpoints(this WebApplication app)
         {
-            app.MapGet("/FundService/GetFundList", async ([FromServices] IFundAdminBL FundBL) =>
+            app.MapGet("/FundService/GetFundPL", async ([FromServices] IFundAdminBL FundBL) =>
             {
                 return await FundBL.GetFundPL();
             });
